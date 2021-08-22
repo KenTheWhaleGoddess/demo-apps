2689fbed0b208a19b9a325a589f1a921338c8039
Ken Preiser <kenpreiser@gmail.com>
2021-08-21 22:36:42 -0700

    Add files via upload

--- moralis-sign-in-boilerplate/main.js
+++ moralis-sign-in-boilerplate/main.js
@@ -3,15 +3,21 @@ Moralis.initialize('igDYNSIRB7leqHRpnUuNPfplY1fn0Y60fp5AM8zV');
 //Server url from moralis.io
 Moralis.serverURL = 'https://e1qealego843.moralisweb3.com:2053/server';
 
-
 async function login() {
     try {
+        await ethereum.enable();
         user = await Moralis.Web3.authenticate();
         console.log(user);
-        alert("User logged in")
+        alert("User logged in");
+        hideLogin();
+        renderRugs();
     } catch (error) {
         console.log(error);
     }
+}
+
+async function hideLogin() {
+	document.getElementById("login_button").hide();
 }
 
 document.getElementById("login_button").onclick = login;
