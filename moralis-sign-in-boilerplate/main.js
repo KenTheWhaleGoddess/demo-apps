7fe889fb6cf4904369b0150e0e82645df4351bdf
Ken Preiser <kenpreiser@gmail.com>
2021-08-21 23:16:47 -0700

    Add files via upload

--- moralis-sign-in-boilerplate/main.js
+++ moralis-sign-in-boilerplate/main.js
@@ -31,10 +31,17 @@ async function renderRugs() {
 	$.each(userEthNFTs, function(i, v) {
 	    if (v.token_address == "0x495f947276749ce646f68ac8c248420045cb7b5e") {
 	        console.log(v.token_id);
-	        alert(v.tokenId);
+	        console.log(getMetadata(v.token_uri));
 	        return;
 	    }
 });	$('#rugs').show();
+}
+
+const getMetadata = async (uri) => {
+  const response = await fetch('uri');
+  const myJson = await response.json(); //extract JSON from the http response
+  console.log(myJson);
+  return myJson;
 }
 
 document.getElementById("login_button").onclick = login;
